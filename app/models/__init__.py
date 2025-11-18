2bfbbbd471261fdf946035758c6cbc58e40e3761
am3696-dev <am3696@njit.edu>
2025-11-17 19:25:58 -0500

    Updated init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,6 @@
 # app/models/__init__.py
 
-# This file ensures that all models are imported and known to SQLAlchemy's Base
-# before they are used, preventing relationship errors.
-
-# Import Base from user.py first, as calculation.py depends on it
-from .user import Base
 from .user import User
 from .calculation import Calculation
+
+__all__ = ["User", "Calculation"]
